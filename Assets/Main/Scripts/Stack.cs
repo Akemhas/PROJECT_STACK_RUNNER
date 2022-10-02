27820df84c8cff87409dd23f1519f4d423f36105
Akemhas <61284057+Akemhas@users.noreply.github.com>
2022-10-02 23:03:28 +0300

    [Added] Cut Mechanics

--- Assets/Main/Scripts/Stack.cs
+++ Assets/Main/Scripts/Stack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,21 +7,38 @@ namespace PROJECT_STACK_RUNNER
 	public class Stack : MonoBehaviour
 	{
 		public Renderer rnd;
-
+		public Rigidbody rb;
 		private Coroutine _moveRoutine;
+
+		private Vector3 XVector => new Vector3(transform.localScale.x,0,0);
+		public float LeftMostCenter => (transform.position - XVector * .5f).x;
+		public float RightMostCenter => (transform.position + XVector * .5f).x;
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere((transform.position - XVector * .5f),.25f);
+			Gizmos.color = Color.green;
+			Gizmos.DrawSphere((transform.position + XVector * .5f),.25f);
+		}
 
 		private void OnValidate()
 		{
 			rnd = GetComponentInChildren<Renderer>();
+			rb = GetComponent<Rigidbody>();
 		}
 
-		public void MoveX(float endTarget,float duration,int loopTime)
+		public void Stop()
 		{
 			if(_moveRoutine != null) StopCoroutine(_moveRoutine);
-			_moveRoutine = StartCoroutine(MoveRoutine(endTarget,duration,loopTime));
 		}
 
-		private IEnumerator MoveRoutine(float endTarget,float duration,int loopTime)
+		public void MoveX(float endTarget,float duration)
+		{
+			if(_moveRoutine != null) StopCoroutine(_moveRoutine);
+			_moveRoutine = StartCoroutine(MoveXRoutine(endTarget,duration));
+		}
+
+		private IEnumerator MoveXRoutine(float endTarget,float duration)
 		{
 			float elapsedTime = 0;
 			Vector3 startPos = transform.position;
@@ -29,9 +47,17 @@ namespace PROJECT_STACK_RUNNER
 			{
 				yield return null;
 				elapsedTime += Time.deltaTime;
-				transform.position = Vector3.Lerp(startPos,targetPos,elapsedTime / duration);
+				transform.position = Vector3.Lerp(startPos,targetPos,InOutSine(elapsedTime / duration));
+
+				if(elapsedTime >= duration)
+				{
+					elapsedTime = 0;
+					startPos = transform.position;
+					targetPos.x *= -1;
+				}
 			}
 			_moveRoutine = null;
 		}
+		private static float InOutSine(float t) => (float)(Math.Cos(t * Math.PI) - 1) / -2;
 	}
 }
--- Assets/Main/Scripts/StackController.cs
+++ Assets/Main/Scripts/StackController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
+
 namespace PROJECT_STACK_RUNNER
 {
 	public class StackController : MonoBehaviour
 	{
+
 		#region Singleton
 
 		private static readonly object s_Lock = new object();
@@ -22,10 +28,148 @@ namespace PROJECT_STACK_RUNNER
 		}
 
 		#endregion
-		
+
 		[Header("Spawn Points")]
 		[SerializeField] private Transform leftSpawnPoint;
 		[SerializeField] private Transform rightSpawnPoint;
-		[SerializeField][Space] private Stack stackPrefab;
+		[SerializeField] [Space] private Stack stackPrefab;
+		[SerializeField] private Vector3 startPos = new Vector3(0,-.25f,0f);
+		[SerializeField] private Material[] stackMaterials;
+		[Space] public List<Stack> stacks = new List<Stack>();
+
+
+		private Vector3 _localScale;
+		private Vector3 NextPos => startPos + new Vector3(0,0,_localScale.z * stacks.Count);
+		private ObjectPool<Stack> _stackPool;
+		public ObjectPool<Stack> ReplacePool;
+
+		private void Awake()
+		{
+			_localScale = stackPrefab.transform.localScale;
+			_stackPool = new ObjectPool<Stack>
+			(
+				() => Instantiate(stackPrefab),
+				stack =>
+				{
+					stack.gameObject.SetActive(true);
+					stack.rnd.sharedMaterial = stackMaterials[stacks.Count % stackMaterials.Length];
+					stacks.Add(stack);
+				},
+				stack =>
+				{
+					stack.gameObject.SetActive(false);
+					stack.transform.localScale = stackPrefab.transform.localScale;
+					stacks.Remove(stack);
+				},null,true,20,100
+			);
+			ReplacePool = new ObjectPool<Stack>
+			(
+				() => Instantiate(stackPrefab),
+				stack =>
+				{
+					stack.gameObject.SetActive(true);
+					stack.rnd.sharedMaterial = stackMaterials[(stacks.Count - 1) % stackMaterials.Length];
+					stack.rb.isKinematic = false;
+				},
+				stack =>
+				{
+					stack.gameObject.SetActive(false);
+					stack.transform.localScale = stackPrefab.transform.localScale;
+					stack.rb.isKinematic = true;
+					stack.rb.velocity = Vector3.zero;
+					stack.rb.angularVelocity = Vector3.zero;
+				},null,false,10,20
+			);
+		}
+
+		private void Start()
+		{
+			SpawnStack(NextPos,false);
+			SpawnStack(NextPos,true);
+		}
+
+		private void Update()
+		{
+			if(!Input.GetMouseButtonDown(0)) return;
+			if(!CutStack()) {}
+			else
+			{
+				SpawnStack(NextPos,true);
+				Player.Instance.Move(stacks[^2].transform.position);
+			}
+		}
+
+		private bool CutStack()
+		{
+			Stack currentStack = stacks[^1];
+			Stack lastStack = stacks[^2];
+			currentStack.Stop();
+
+			if(currentStack.LeftMostCenter > lastStack.RightMostCenter || currentStack.RightMostCenter < lastStack.LeftMostCenter)
+			{
+				Fail();
+				return false;
+			}
+			Transform currentStackTransform = currentStack.transform;
+			Vector3 pos = currentStackTransform.position;
+			Vector3 scale = currentStackTransform.localScale;
+			float diff;
+
+			diff = currentStack.LeftMostCenter - lastStack.LeftMostCenter;
+
+			if(diff < 0)
+			{
+				diff = lastStack.LeftMostCenter - currentStack.LeftMostCenter;
+
+				Stack replaceStack = ReplacePool.Get();
+				Transform replaceTransform = replaceStack.transform;
+
+				replaceStack.gameObject.SetActive(true);
+				replaceTransform.position = new Vector3(lastStack.LeftMostCenter - diff * .5f,pos.y,pos.z);
+				replaceTransform.localScale = new Vector3(diff,scale.y,scale.z);
+				replaceStack.rb.isKinematic = false;
+
+				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - diff,scale.y,scale.z);
+				currentStackTransform.position = new Vector3(lastStack.LeftMostCenter + currentStackTransform.localScale.x * .5f,pos.y,pos.z);
+			}
+
+			diff = currentStack.RightMostCenter - lastStack.RightMostCenter;
+
+			if(diff > 0)
+			{
+				Stack replaceStack = ReplacePool.Get();
+				Transform replaceTransform = replaceStack.transform;
+
+				replaceStack.gameObject.SetActive(true);
+				replaceTransform.position = new Vector3(lastStack.RightMostCenter + diff * .5f,pos.y,pos.z);
+				replaceTransform.localScale = new Vector3(diff,scale.y,scale.z);
+				replaceStack.rb.isKinematic = false;
+
+				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - diff,scale.y,scale.z);
+				currentStackTransform.position = new Vector3(lastStack.RightMostCenter - currentStackTransform.localScale.x * .5f,pos.y,pos.z);
+			}
+
+			return true;
+		}
+
+		private void Fail() {}
+
+		private Stack SpawnStack(Vector3 spawnPos,bool shouldMove)
+		{
+			Stack stack = _stackPool.Get();
+			if(shouldMove) RandomMove(stack,spawnPos);
+			else stack.transform.position = spawnPos;
+			return stack;
+		}
+
+		private void RandomMove(Stack stack,Vector3 spawnPos)
+		{
+			int random = Random.Range(1,3);
+			bool isLeft = random % 2 == 0;
+			(Vector3,Vector3) points = (leftSpawnPoint.position,rightSpawnPoint.position);
+			if(!isLeft) (points.Item1,points.Item2) = (points.Item2,points.Item1);
+			stack.transform.position = spawnPos + points.Item1;
+			stack.MoveX(points.Item2.x,2);
+		}
 	}
 }
