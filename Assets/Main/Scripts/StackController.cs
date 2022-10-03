13a7b35132b1739f7c682a8187e706e77826dac3
Akemhas <bozdemirae@gmail.com>
2022-10-03 23:38:56 +0300

    [Added] Sound Effect

--- Assets/Main/Scripts/StackController.cs
+++ Assets/Main/Scripts/StackController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 namespace PROJECT_STACK_RUNNER
@@ -33,20 +35,27 @@ namespace PROJECT_STACK_RUNNER
 		[SerializeField] private Transform leftSpawnPoint;
 		[SerializeField] private Transform rightSpawnPoint;
 		[SerializeField] [Space] private Stack stackPrefab;
-		[SerializeField] private Vector3 startPos = new Vector3(0,-.25f,0f);
+		[SerializeField] private FinishTrigger finishPrefab;
+		[SerializeField] private int stackCountToReachFinish = 15;
+		[SerializeField] internal Vector3 startPos = new Vector3(0,-.25f,0f);
+		[SerializeField] private float errorTolerance = .2f;
 		[SerializeField] private Material[] stackMaterials;
+		[SerializeField] private AudioSource audioSource;
 		[Space] public List<Stack> stacks = new List<Stack>();
+		private int _comboCount;
 
-
+		private FinishTrigger _finishTrigger;
+		private bool _isLeft;
+		public bool canWalk;
+		private float finishOffset;
 		private Vector3 _localScale;
-		private Vector3 NextPos => startPos + new Vector3(0,0,_localScale.z * stacks.Count);
-		private ObjectPool<Stack> _stackPool;
-		public ObjectPool<Stack> ReplacePool;
+		public Vector3 NextPos => startPos + new Vector3(0,0,_localScale.z * stacks.Count);
+		public ObjectPool<Stack> StackPool,ReplacePool;
 
 		private void Awake()
 		{
 			_localScale = stackPrefab.transform.localScale;
-			_stackPool = new ObjectPool<Stack>
+			StackPool = new ObjectPool<Stack>
 			(
 				() => Instantiate(stackPrefab),
 				stack =>
@@ -59,12 +68,18 @@ namespace PROJECT_STACK_RUNNER
 				{
 					stack.gameObject.SetActive(false);
 					stack.transform.localScale = stackPrefab.transform.localScale;
+					stack.ResetStack();
 					stacks.Remove(stack);
 				},null,true,20,100
 			);
 			ReplacePool = new ObjectPool<Stack>
 			(
-				() => Instantiate(stackPrefab),
+				() =>
+				{
+					Stack replaceStack = Instantiate(stackPrefab);
+					replaceStack.IsReplace = true;
+					return replaceStack;
+				},
 				stack =>
 				{
 					stack.gameObject.SetActive(true);
@@ -75,15 +90,16 @@ namespace PROJECT_STACK_RUNNER
 				{
 					stack.gameObject.SetActive(false);
 					stack.transform.localScale = stackPrefab.transform.localScale;
-					stack.rb.isKinematic = true;
-					stack.rb.velocity = Vector3.zero;
-					stack.rb.angularVelocity = Vector3.zero;
+					stack.ResetStack();
 				},null,false,10,20
 			);
 		}
 
 		private void Start()
 		{
+			finishOffset = stackPrefab.transform.localScale.z * stackCountToReachFinish;
+			Vector3 finishPos = new Vector3(0,0,finishOffset - .35f);
+			_finishTrigger = Instantiate(finishPrefab,finishPos,Quaternion.identity);
 			SpawnStack(NextPos,false);
 			SpawnStack(NextPos,true);
 		}
@@ -91,12 +107,47 @@ namespace PROJECT_STACK_RUNNER
 		private void Update()
 		{
 			if(!Input.GetMouseButtonDown(0)) return;
-			if(!CutStack()) {}
+			if(!canWalk) return;
+			if(!CutStack())
+			{
+				if(!canWalk) return;
+				SpawnAgain();
+			}
 			else
 			{
-				SpawnStack(NextPos,true);
-				Player.Instance.Move(stacks[^2].transform.position);
+				if(stacks.Count < stackCountToReachFinish)
+				{
+					SpawnStack(NextPos,true);
+					Player.Instance.Move(stacks[^2].transform.position);
+				}
+				else
+				{
+					Player.Instance.Move(_finishTrigger.transform.position);
+				}
+
 			}
+		}
+
+		public FinishTrigger SpawnFinishLine()
+		{
+			Vector3 finishPos = new Vector3(0,0,_finishTrigger.transform.position.z + finishOffset - .35f);
+			_finishTrigger = Instantiate(finishPrefab,finishPos,Quaternion.identity);
+			return _finishTrigger;
+		}
+
+		private void SpawnAgain()
+		{
+			Stack currentStack = stacks[^1];
+			Transform stackTransform = currentStack.transform;
+			Stack replaceStack = ReplacePool.Get();
+			Transform replaceTransform = replaceStack.transform;
+			replaceTransform.position = stackTransform.position;
+			replaceTransform.localScale = stackTransform.localScale;
+			replaceStack.gameObject.SetActive(true);
+			replaceStack.Fall();
+
+			StackPool.Release(currentStack);
+			SpawnStack(NextPos);
 		}
 
 		private bool CutStack()
@@ -105,69 +156,104 @@ namespace PROJECT_STACK_RUNNER
 			Stack lastStack = stacks[^2];
 			currentStack.Stop();
 
-			if(currentStack.LeftMostCenter > lastStack.RightMostCenter || currentStack.RightMostCenter < lastStack.LeftMostCenter)
-			{
-				Fail();
-				return false;
-			}
+			if(currentStack.LeftMostCenter > lastStack.RightMostCenter || currentStack.RightMostCenter < lastStack.LeftMostCenter) return false;
+
 			Transform currentStackTransform = currentStack.transform;
 			Vector3 pos = currentStackTransform.position;
 			Vector3 scale = currentStackTransform.localScale;
-			float diff;
 
-			diff = currentStack.LeftMostCenter - lastStack.LeftMostCenter;
+			float leftDiff = currentStack.LeftMostCenter - lastStack.LeftMostCenter;
 
-			if(diff < 0)
+			if(Mathf.Abs(leftDiff) <= errorTolerance)
 			{
-				diff = lastStack.LeftMostCenter - currentStack.LeftMostCenter;
+				_comboCount++;
+				var iLerp = Mathf.InverseLerp(0,15,_comboCount);
+				audioSource.pitch = Mathf.Lerp(.5f,1.25f,iLerp);
+				audioSource.Play();
+				currentStackTransform.localScale = lastStack.transform.localScale;
+				currentStackTransform.position = new Vector3(lastStack.transform.position.x,pos.y,pos.z);
+			}
+			else
+			{
+				_comboCount = 0;
+			}
+
+			if(leftDiff < 0)
+			{
+				leftDiff = lastStack.LeftMostCenter - currentStack.LeftMostCenter;
 
 				Stack replaceStack = ReplacePool.Get();
 				Transform replaceTransform = replaceStack.transform;
 
 				replaceStack.gameObject.SetActive(true);
-				replaceTransform.position = new Vector3(lastStack.LeftMostCenter - diff * .5f,pos.y,pos.z);
-				replaceTransform.localScale = new Vector3(diff,scale.y,scale.z);
-				replaceStack.rb.isKinematic = false;
+				replaceTransform.position = new Vector3(lastStack.LeftMostCenter - leftDiff * .5f,pos.y,pos.z);
+				replaceTransform.localScale = new Vector3(leftDiff,scale.y,scale.z);
+				replaceStack.Fall();
 
-				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - diff,scale.y,scale.z);
+				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - leftDiff,scale.y,scale.z);
 				currentStackTransform.position = new Vector3(lastStack.LeftMostCenter + currentStackTransform.localScale.x * .5f,pos.y,pos.z);
 			}
 
-			diff = currentStack.RightMostCenter - lastStack.RightMostCenter;
+			float rightDiff = currentStack.RightMostCenter - lastStack.RightMostCenter;
 
-			if(diff > 0)
+			if(rightDiff > 0)
 			{
 				Stack replaceStack = ReplacePool.Get();
 				Transform replaceTransform = replaceStack.transform;
 
 				replaceStack.gameObject.SetActive(true);
-				replaceTransform.position = new Vector3(lastStack.RightMostCenter + diff * .5f,pos.y,pos.z);
-				replaceTransform.localScale = new Vector3(diff,scale.y,scale.z);
-				replaceStack.rb.isKinematic = false;
+				replaceTransform.position = new Vector3(lastStack.RightMostCenter + rightDiff * .5f,pos.y,pos.z);
+				replaceTransform.localScale = new Vector3(rightDiff,scale.y,scale.z);
+				replaceStack.Fall();
 
-				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - diff,scale.y,scale.z);
+				currentStackTransform.localScale = new Vector3(currentStackTransform.localScale.x - rightDiff,scale.y,scale.z);
 				currentStackTransform.position = new Vector3(lastStack.RightMostCenter - currentStackTransform.localScale.x * .5f,pos.y,pos.z);
+			}
+
+			if(currentStackTransform.localScale.x < errorTolerance)
+			{
+				StackPool.Release(currentStack);
+				currentStack.Fall();
+				canWalk = false;
+				SceneManager.LoadSceneAsync(0);
+				return false;
 			}
 
 			return true;
 		}
 
-		private void Fail() {}
-
-		private Stack SpawnStack(Vector3 spawnPos,bool shouldMove)
+		public Stack SpawnStack(Vector3 spawnPos,bool shouldMove)
 		{
-			Stack stack = _stackPool.Get();
-			if(shouldMove) RandomMove(stack,spawnPos);
+			Stack stack = StackPool.Get();
+			if(shouldMove)
+			{
+				stack.transform.localScale = stacks[^2].transform.localScale;
+				RandomMove(stack,spawnPos);
+			}
 			else stack.transform.position = spawnPos;
+			return stack;
+		}
+
+		public Stack SpawnStack(Vector3 spawnPos)
+		{
+			Stack stack = StackPool.Get();
+			stack.transform.localScale = stacks[^2].transform.localScale;
+			var position = leftSpawnPoint.position;
+			float dist = Vector3.Distance(stacks[^1].transform.position,position);
+			(Vector3,Vector3) points = dist > Mathf.Abs(position.x)
+				? (leftSpawnPoint.position,rightSpawnPoint.position)
+				: (rightSpawnPoint.position,leftSpawnPoint.position);
+			stack.transform.position = spawnPos + points.Item1;
+			stack.MoveX(points.Item2.x,2);
 			return stack;
 		}
 
 		private void RandomMove(Stack stack,Vector3 spawnPos)
 		{
 			int random = Random.Range(1,3);
-			bool isLeft = random % 2 == 0;
+			_isLeft = random % 2 == 0;
 			(Vector3,Vector3) points = (leftSpawnPoint.position,rightSpawnPoint.position);
-			if(!isLeft) (points.Item1,points.Item2) = (points.Item2,points.Item1);
+			if(!_isLeft) (points.Item1,points.Item2) = (points.Item2,points.Item1);
 			stack.transform.position = spawnPos + points.Item1;
 			stack.MoveX(points.Item2.x,2);
 		}
